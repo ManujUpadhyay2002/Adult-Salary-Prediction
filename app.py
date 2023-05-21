ab12d23fa541b803da047840f13dab09195beb3b
Manuj Upadhyay <85246907+ManujUpadhyay2002@users.noreply.github.com>
2023-05-22 00:12:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,20 @@ dict_data = {'Preschool': 1, '1st-4th': 2, '5th-6th': 3, '7th-8th': 4, '9th': 5,
              'Some-college': 10, 'Assoc-voc': 11, 'Assoc-acdm': 12, 'Bachelors': 13, 'Masters': 14, 'Prof-school': 15, 'Doctorate': 16}
 
 age = st.text_input('Your Age', )
-st.write('Your Age : ', age)
 
 workclass = st.selectbox(
     'Select Your Work Class',
     ('Private', 'Local-gov', 'other', 'Self-emp-not-inc', 'Federal-gov',
      'State-gov', 'Self-emp-inc', 'Without-pay', 'Never-worked'))
-st.write('You selected:', workclass)
 
 education = st.selectbox(
     'Select Your Education',
     ('Preschool', '1st-4th', '5th-6th', '7th-8th', '9th', '10th', '11th', '12th', 'HS-grad', 'Some-college', 'Assoc-voc', 'Assoc-acdm', 'Bachelors', 'Masters', 'Prof-school', 'Doctorate'))
-st.write('You selected:',dict_data[education])
 
 maritalstatus = st.selectbox(
     'Select Your maritalstatus ',
     ('Never-married', 'Married-civ-spouse', 'Widowed', 'Divorced',
      'Separated', 'Married-spouse-absent', 'Married-AF-spouse'))
-st.write('Your Status:',maritalstatus)
 
 occupation = st.selectbox(
     'Select Your occupation',
@@ -33,27 +29,22 @@ occupation = st.selectbox(
      'Other-service', 'Prof-specialty', 'Craft-repair', 'Adm-clerical',
      'Exec-managerial', 'Tech-support', 'Sales', 'Priv-house-serv',
      'Transport-moving', 'Handlers-cleaners', 'Armed-Forces'))
-st.write('You selected:', occupation)
 
 relationship = st.selectbox(
     'Select Your relationship',
     ('Own-child', 'Husband', 'Not-in-family', 'Unmarried', 'Wife',
      'Other-relative'))
-st.write('You selected:', relationship)
 
 race = st.selectbox(
     'Select Your race',
     ('Black', 'White', 'Asian-Pac-Islander', 'Other',
      'Amer-Indian-Eskimo'))
-st.write('You selected:', race)
 
 gender = st.selectbox(
     'Select Your gender',
     ('Male', 'Female'))
-st.write('You selected:', gender)
 
 h_p_w = st.text_input('Hours per week you work', )
-st.write('Hours per week you work : ', h_p_w)
 
 data = pd.DataFrame([age,workclass,dict_data[education],maritalstatus,occupation,relationship,race,gender,h_p_w]).transpose()
 
